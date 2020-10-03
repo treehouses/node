0dd5a8fb27d2aba263f86fecf398c8007812e084
Anna X <vmnet8@gmail.com>
2020-10-02 23:01:43 -0700

    delete extra lines

--- docker-build.sh
+++ docker-build.sh
@@ -6,8 +6,6 @@ if [ $# -le 1 ]; then
   exit 1
 fi
 
-#dir=$(dirname $0)
-#sha=$($dir/manifest-alpine-sha.sh $@)   # $1 vmnet8/alpine:latest  amd64|arm|arm64
 sha=$(get_manifest_sha $@)
 echo $sha
 base_image="treehouses/alpine@$sha"
@@ -18,6 +16,4 @@ if [ -n "$sha" ]; then
   tag=treehouses/node-tags:$arch
   sed "s|{{base_image}}|$base_image|g" Dockerfile.template > Dockerfile.$arch
   docker build -t $tag -f Dockerfile.$arch .
-  #version=$(docker run -it $tag /bin/sh -c "nginx -v" |awk '{print$3}')
-  #echo "$arch nginx version is $version"
 fi
